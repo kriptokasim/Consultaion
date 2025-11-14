686b707d7faf5f83bcc6006c379433ca3d4c81b7
Horhacker <kriptokasim@gmail.com>
2025-11-14 10:58:20 -0500

    chore(types): add stub modules for fs/path

--- apps/web/types/global.d.ts
+++ apps/web/types/global.d.ts
@@ -9,3 +9,13 @@ declare namespace NodeJS {
 }
 
 declare var process: NodeJS.Process;
+
+declare module "fs" {
+  const fs: any;
+  export default fs;
+}
+
+declare module "path" {
+  const path: any;
+  export default path;
+}
