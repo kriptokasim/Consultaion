a4b45cf33eb9e4297b46b87d5d836688835169fd
Horhacker <kriptokasim@gmail.com>
2026-06-04 12:52:03 -0400

    migration: add gateway_policy to debate table

---
+++ apps/api/alembic/versions/p113_add_gateway_policy.py
@@ -0,0 +1,27 @@
+"""Add gateway_policy to debate
+
+Revision ID: p113_add_gateway_policy
+Revises: p112_idx_001
+Create Date: 2026-06-04
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+import sqlmodel
+
+revision: str = "p113_add_gateway_policy"
+down_revision: Union[str, None] = "p112_idx_001"
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    with op.batch_alter_table("debate", schema=None) as batch_op:
+        batch_op.add_column(sa.Column("gateway_policy", sqlmodel.sql.sqltypes.AutoString(), nullable=True))
+
+
+def downgrade() -> None:
+    with op.batch_alter_table("debate", schema=None) as batch_op:
+        batch_op.drop_column("gateway_policy")
